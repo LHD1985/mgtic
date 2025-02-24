11e21661311ad3ace5999e0cd5dadb1042143506
LHD1985 <delgado168082@uabc.edu.mx>
2025-02-23 21:08:01 -0800

    terceraversion

---
+++ scrips1.js
@@ -0,0 +1,32 @@
+// keyword variableName = value;
+// var, let, const
+
+//string
+var firstName = Lupita";
+var lastName= "Haros";
+
+//concatenation
+
+console.log("Name: "+firstName + "" + lasName);
+
+firstName = "Lupita";
+
+//numerical
+let myAge=99;
+let myHeight=100;
+
+console.log(myAge,myHeigth);
+
+//boolean (true/false)
+let isStudent  = true;
+
+//math operations + - / *"
+let num=;
+let mum=12;
+
+
+console.long "El resultado es = " +
+
+
+
+
